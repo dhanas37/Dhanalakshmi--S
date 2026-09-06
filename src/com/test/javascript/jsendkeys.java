35b2f406a7678bfba054ee816f9c3832dfa7baec
dhanas37 <dhanasr96@gmail.com>
2026-09-06 14:41:01 +0530

    Reupdates js files

--- src/com/test/javascript/jsendkeys.java
+++ src/com/test/javascript/jsendkeys.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 //javascript
+//Dhana
+//Dhana
 public class jsendkeys {
 	public static void main(String[] args) throws InterruptedException {
 		WebDriver driver =new ChromeDriver();
